0029c32bd0080b6a5698f516d7cc13dfecd7a31d
trong.ngotrung@gmail.com <trong.ngotrung@gmail.com>
2019-08-09 00:56:26 +0700

    built-in pipes

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './components/hello/hello.component';
-import { HiComponent } from './components/hi/hi.component';
+import { BuiltInPipeComponent } from './components/built-in-pipes/buit-in-pipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
-    HiComponent
+    BuiltInPipeComponent
   ],
   imports: [
     BrowserModule
